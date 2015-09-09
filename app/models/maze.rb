a01fe0f521779c84ee5a98e52e8e05f6472488c8
Stephen Dicks <stephen@starswan.com>
2015-09-09 17:42:00 +0100

    completed ruby part of test

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -4,12 +4,53 @@ class Maze < ActiveRecord::Base
 
   has_many :maze_walls, :dependent => :destroy
 
+  Wall = Struct.new :down, :right
+
   after_create do |maze|
-    logger.debug "After create #{maze.inspect}"
+    walls = {}
     1.upto(maze.height) do |yposition|
       1.upto(maze.width) do |xposition|
-        maze.maze_walls.create! :xposition => xposition, :yposition => yposition, :down => true, :right => true
+        walls[[xposition, yposition]] = Wall.new(true, true)
       end
     end
+    xpos, ypos = 1, 1
+    visited_cells = Set.new [[xpos, ypos]]
+    cellstack = []
+    while visited_cells.size < walls.size
+      neighbours = [[xpos-1, ypos], [xpos+1, ypos], [xpos, ypos-1], [xpos, ypos+1]].reject do |x, y|
+        x < 1 or y < 1 or x > maze.width or y > maze.height
+      end
+      unvisited_neighbours = neighbours.reject { |x, y| visited_cells.include? [x, y] }
+      if unvisited_neighbours.size == 0
+        xpos, ypos = cellstack.pop
+      else
+        cellstack << [xpos, ypos]
+        newx, newy = unvisited_neighbours[rand(unvisited_neighbours.size)]
+        # Remove wall between current and chosen
+        if newx != xpos
+          # need to remove a right wall (ypos === newy)
+          if newx < xpos
+            # remove newx
+            walls[[newx, ypos]].right = false
+          else
+            # remove xpos
+            walls[[xpos, ypos]].right = false
+          end
+        else
+          # need to remove a down wall (xpos === newx)
+          if newy < ypos
+            # remove newy
+            walls[[xpos, newy]].down = false
+          else
+            # remove ypos
+            walls[[xpos, ypos]].down = false
+          end
+        end
+        xpos, ypos = newx, newy
+        visited_cells << [xpos, ypos]
+      end
+    end
+    walls.each { |key, data| maze.maze_walls.create! :xposition => key[0], :yposition => key[1],
+                                                     :down => data.down, :right => data.right }
   end
 end
--- spec/models/maze_spec.rb
+++ spec/models/maze_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Maze, :type => :model do
   let(:valid_attributes) {
-    { :width => 3, :height => 2 }
+    { :width => 3, :height => 3 }
   }
 
   describe "creating a new maze" do
     it "should generate a solvable maze starting from 1,1 with a recognized exit" do
       maze = Maze.create! valid_attributes
       walls = maze.maze_walls
-      expect(walls.count).to eq(6)
+      expect(walls.count).to eq(9)
       maze_exit = walls.find { |wall| wall.xposition == 1 and wall.yposition == 1 }
       expect(maze_exit.xposition).to eq(1)
       expect(maze_exit.yposition).to eq(1)
+
+      expect(walls.select { |w| not w.down or not w.right }.size).to be > 0
+      # expect(walls.select { |w| not w.down or not w.right }.size).to eq(5)
+      # expect(walls).to eq([])
 
       maze_hash = walls.inject({}) do |hash, wall|
         hash[[wall.xposition, wall.yposition]] = {:down => wall.down, :right => wall.right}
@@ -22,20 +26,21 @@ RSpec.describe Maze, :type => :model do
       xpos, ypos = 1, 1
       # Try to solve maze, backtracking if the path is a dead end
       solution = [] << [xpos, ypos]
-      visited_hash = {}
-      visited_hash[[xpos, ypos]] = true
-      while visited_hash.size < walls.size and solution.size > 0
+      visited_set = Set.new [[xpos, ypos]]
+      while visited_set.size < walls.size and solution.size > 0
         # Need to find an accessible non-visited cell
-        neighbours = [[xpos-1, ypos, :right], [xpos+1, ypos, :right], [xpos, ypos-1, :down], [xpos, ypos+1, :down]]
+        neighbours = [[-1, 0, :right, -1, 0], [1, 0, :right, 0, 0], [0, -1, :down, 0, -1], [0, 1, :down, 0, 0]]
         # Rely on maze_hash only having valid positions e.g. with co-ordinates 1 and above
-        non_visited_neighbours = neighbours.reject { |x, y, dir| visited_hash.has_key? [x,y] }
-                                           .select { |x, y, dir| x = maze_hash[[x, y]]; x and not x[dir]}
+        non_visited_neighbours = neighbours.reject do |x, y|
+          visited_set.include? [xpos + x,ypos + y]
+        end.select { |x, y, dir, xo, yo| mh = maze_hash[[xpos + xo, ypos + yo]]; mh and not mh[dir]}
         if non_visited_neighbours.size == 0
           # Back track
           xpos, ypos = solution.pop
         else
-          xpos, ypos, direction = non_visited_neighbours[rand(non_visited_neighbours.size)]
-          visited_hash[[xpos, ypos]] = true
+          xoffset, yoffset = non_visited_neighbours[rand(non_visited_neighbours.size)]
+          xpos, ypos = xpos + xoffset, ypos + yoffset
+          visited_set << [xpos, ypos]
           solution << [xpos, ypos]
         end
       end
