ae247f43396bda5db426573ca54c4f2a6860ae1d
Stephen Dicks <stephen@starswan.com>
2015-09-11 10:04:37 +0100

    Improved svg rendering test by counting walls

--- spec/views/walls/index.svg.builder_spec.rb
+++ spec/views/walls/index.svg.builder_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe "walls/index.svg.builder", type: :view do
   before(:each) do
     assign(:maze, Maze.create!(height: 2, width: 2))
-    assign(:walls, Maze.last.walls)
+    @walls = assign(:walls, Maze.last.walls)
   end
 
   it 'renders svg data' do
     render
     # The exact number of walls will depend on the solution...
-    assert_select 'svg>line', :minimum => 2
+    # But there will always be at least 2 walls top and left
+    wall_count = 2 + @walls.collect { |w| (w.down ? 1 : 0) + (w.right ? 1 : 0) }.sum
+    assert_select 'svg>line', :count => wall_count
   end
 
 end
